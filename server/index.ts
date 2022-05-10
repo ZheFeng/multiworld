4e43023ca3199b4b3e112b3d5edc16f837967065
Zhe Feng <zhe@jumiduo.com>
2022-05-10 13:15:05 +0800

    enable server call app js

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,11 @@
 import express from 'express'
+import * as path from 'path'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send(`
-    <html>
-        <head>
-            <meta charset="utf-8"/>
-        </head>
-        <body>
-            <div id="app"></div>
-            <script src="https://multiworld.oss-cn-beijing.aliyuncs.com/dist/app.bundle.js"></script>
-            <script type="text/javascript">
-            window.start('app');
-            </script>
-        </body>
-    </html>
-  `)
-})
+app.use('/', express.static(path.join(__dirname, '../dist')))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
